2a9eeb1798809fce041c6f6e53d6510ac27bd65a
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-07-26 15:31:46 -0700

    Begin migration from status site to node site/server. Added npm packages and package.json. Begin to implement ejs templating engine

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/');
+
+app.listen(3000);
